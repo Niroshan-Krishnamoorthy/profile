bb3fc092c68883bef2f62b104052b863623c2d32
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2020-01-24 14:14:47 +0530

    adding components

---
+++ src/components/Left/Left.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Left.css'
+function Left() {
+  return (
+    <div className="App">
+        <div className="left">
+            <div className="bg-img"></div>
+            <div className="leftInner">
+                <div className="info">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Left;
